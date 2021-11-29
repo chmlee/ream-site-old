469af8e4460ad42c4300f55be32a891407d91d8d
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-11-29 07:10:52 -0600

    add vite-plugin-wasm-pack

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
 
+import wasmPack from 'vite-plugin-wasm-pack';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+        ssr: false,
+
+        vite: () => ({
+            plugins: [
+                wasmPack('./ream')
+            ]
+        })
 	}
 };
 
