abd7cc227b5fae84077311041fa5883bf3a8645c
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-11-30 02:34:44 -0600

    add ream

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import init, { ream2csv } from 'ream';
+console.log(1)
+init().then(() => {
+    const input = `
+    # Data
+    - key: value
+    - k: v
+    - one: 1
+    - two: 2
+    - three: 3
+
+    ## Child
+    - one: 1
+    - two: 2
+
+    ## Child
+    - one: 3
+    - two: 4
+    `
+    const result = ream2csv(input)
+    console.log(result)
+})
+</script>
+
+
+<div class="main">
+    <div class="intro">
+        Data language for building maintainble and reuable social science dataset
+    </div>
+
+    <div class="index-button">
+        <a href="">Why REAM</a>
+        <a href="">Get Started</a>
+    </div>
+
+    <div class="box note">
+        <div class="box-title">Note</div>
+        <p>
+            The project is under active development and is not production ready.
+            Youa re free to play around with the language and tools, but note that backward compatibility is not guaranteed.
+            If you are interested in using REAM for serious projects, please 
+            <a href="mailto:louis@chihminglee.com">contact me</a>
+        </p>
+    </div>
+
+    <div>
+    </div>
+</div>
